71437dbecc2062a14b1f6acb9f312b9d4946e0ab
Otavio Brito <otaviofbrito@gmail.com>
2022-02-01 20:54:27 -0300

    accordion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const getBtn = document.querySelectorAll('.qa_bt')
-const card = document.querySelectorAll('.qa_card')
+const card = document.getElementsByClassName('qa_card')
+const toggle = document.getElementsByClassName('qa_bt')
 
-for (const btn of getBtn) {
-  btn.addEventListener('click', function(){
-    card.classList.remove('active')
+for (i = 0; i<toggle.length; i++){
+  toggle[i].addEventListener('click', function(){
+    this.classList.toggle('active')
   })
 }
